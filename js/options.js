4e31418cf38d2bba7937a79a272ac05b5aeebedc
msrachellouise <r.salby@latrobe.edu.au>
2020-07-09 08:02:39 +1000

    adding

---
+++ js/options.js
@@ -0,0 +1,15 @@
+  (function() {
+  var cx = "010391021890985609619:txywclsdsx4";
+  var gcse = document.createElement("script");
+  gcse.type = "text/javascript";
+  gcse.async = true;
+  gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(gcse, s);
+  })();
+  window.onload = function()
+  {
+  var searchBox =  document.getElementById("gsc-i-id1");
+  searchBox.placeholder="alma troubleshooting";
+  searchBox.title="Search Primo and Alma community documentation";
+  }
